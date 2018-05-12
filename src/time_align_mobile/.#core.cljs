d68316c114beeade385561c00075d6259668a0c4
jgoodhcg <jgoodhcg@gmail.com>
2018-05-12 18:33:22 -0400

    Start the data model

--- src/time_align_mobile/.#core.cljs
+++
@@ -1,1 +0,0 @@
-justin@justin-thinkpad.6069:1526150212
--- src/time_align_mobile/db.cljs
+++ src/time_align_mobile/db.cljs
@@ -7,4 +7,26 @@
   (s/keys :req-un [::greeting]))
 
 ;; initial state of app-db
-(def app-db {:greeting "Hello Clojurescript in Expo!"})
+(def app-db {:tasks [{:id (uuid)
+                      :color ""
+                      :complete false
+                      :label ""
+                      :description ""
+                      :data {}
+                      :periods [{:id
+                                 :start (date-time)
+                                 :stop (date-time)
+                                 :description ""
+                                 :planned false
+                                 :data {}}]}]
+             :templates [{:id (uuid)
+                          :relative-start (time)
+                          :relative-stop (time)
+                          :duration (integer)
+                          :description ""
+                          :planned true
+                          :data {}}]
+             :view {:each-page {:range
+                                :filter
+                                :structured-data}}
+             :navigation {:current-page :keyword}})
