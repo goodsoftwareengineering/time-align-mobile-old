e66f9afde94200522628e233fac6c0cc3fc1771e
justin good <jgoodhcg@gmail.com>
2018-06-19 19:58:37 -0400

    Add untested changes for structured data

--- src/time_align_mobile/screens/task_form.cljs
+++ src/time_align_mobile/screens/task_form.cljs
@@ -4,56 +4,67 @@
                                                   text-input
                                                   touchable-highlight
                                                   switch]]
+            [re-frame.core :refer [subscribe dispatch]]
             [clojure.walk :refer [walk]]))
 
-(defn structured-data [current-path data]
-  ;; breadcrumbs
+(defn map-button [current-path k v]
+  [touchable-highlight
+   {:key      (str "structured-data-field-" k "-" v)
+    :on-press (fn [_]
+                (println (let [new-path (into [k] current-path)]
+                           {:new-path new-path})))}
+   [view
+    [text "button here to go to the nested map"]]])
 
+(defn coll-button [current-path k v]
+  [touchable-highlight
+   {:key      (str "structured-data-field-" k "-" v)
+    :on-press (fn [_]
+                (println (let [new-path (into [k] current-path)]
+                           {:new-path new-path})))}
+   [view
+    [text "button here to go to the collection"]]])
+
+(defn number-input [data current-path k v]
+  [text-input
+   {:default-value  (str v)
+    :style          {:height 40
+                     :width  200}
+    :spell-check    true
+    :on-change-text (fn [new-v]
+                      (println (let [new-path (into [k] current-path)
+                                     new-data (assoc-in data new-path (js/parseFloat new-v))]
+                                 {:new-path new-path
+                                  :new-data new-data})))}])
+
+(defn string-input [data current-path k v]
+  [text-input
+   {:default-value  v
+    :style          {:height 40
+                     :width  200}
+    :spell-check    true
+    :on-change-text (fn [new-v]
+                      (println (let [new-path (into [k] current-path)
+                                     new-data (assoc-in data new-path new-v)]
+                                 {:new-path new-path
+                                  :new-data new-data})))}])
+
+(defn boolean-input [data current-path k v]
+  [switch {:on-value-change (fn [new-v] (println new-v))
+           :value v}])
+
+(defn value-element-picker [v data current-path]
+  (cond
+    (map? v)     (map-button current-path k v)
+    (coll? v)    (coll-button current-path k v)
+    (number? v)  (number-input data current-path k v)
+    (string? v)  (string-input data current-path k v)
+    (boolean? v) (boolean-input data current-path k v)
+    :else        [text "not a supported element"]))
+
+(defn map-input [current-path data subset]
   (walk (fn [[k v]]
-          (let [value-element
-                (cond
-                  (map? v) [touchable-highlight
-                            {:key      (str "structured-data-field-" k "-" v)
-                             :on-press (fn [_]
-                                         (println (let [new-path (into [k] current-path)]
-                                                    {:new-path new-path})))}
-                            [view
-                             [text "button here to go to the nested map"]]]
-
-                  (coll? v) [touchable-highlight
-                             {:key      (str "structured-data-field-" k "-" v)
-                              :on-press (fn [_]
-                                          (println (let [new-path (into [k] current-path)]
-                                                     {:new-path new-path})))}
-                             [view
-                              [text "button here to go to the collection"]]]
-
-                  (number? v) [text-input
-                               {:default-value  (str v)
-                                :style          {:height 40
-                                                 :width  200}
-                                :spell-check    true
-                                :on-change-text (fn [new-v]
-                                                  (println (let [new-path (into [k] current-path)
-                                                                 new-data (assoc-in data new-path (js/parseFloat new-v))]
-                                                             {:new-path new-path
-                                                              :new-data new-data})))}]
-
-                  (string? v) [text-input
-                               {:default-value  v
-                                :style          {:height 40
-                                                 :width  200}
-                                :spell-check    true
-                                :on-change-text (fn [new-v]
-                                                  (println (let [new-path (into [k] current-path)
-                                                                 new-data (assoc-in data new-path new-v)]
-                                                             {:new-path new-path
-                                                              :new-data new-data})))}]
-
-                  (boolean? v) [switch {:on-value-change (fn [new-v] (println new-v))
-                                        :value v}]
-
-                  :else [text "not a supported element"])]
+          (let [value-element (value-element-picker v data current-path)]
 
             [view {:style {:flex 1 :flex-direction "row" :align-items "center"}}
              [text {:style {:color         "grey"
@@ -63,7 +74,22 @@
 
         (fn [elements] (into [view {:style {:flex 1}}] elements))
 
-        (into [] (get-in data current-path data))))
+        (into [] subset)))
+
+(defn collection-input [current-path data subset]
+  [view (map (fn [v] (value-element-picker v data current-path)) subset)])
+
+(defn structured-data [current-path data]
+  ;; TODO breadcrumbs
+
+  (let [subset (get-in data current-path data)]
+
+    (cond
+      (map? subset) (map-input current-path data subset)
+
+      (coll? subset) (collection-input current-path data subset)
+
+      :else [view [text "subset isn't a map or collection"]])))
 
 (defn root [{:keys [task]}]
   (let [task {:id          (random-uuid)
@@ -78,14 +104,16 @@
                             :vector         [1 2 3 "string"]
                             :list           '(1 2 3 4)}
               :color       "#2222aa"
-              :periods     nil}]
+              :periods     nil}
+        ;; current-path (subscribe [:get-task-form-structured-data-current-path])
+        ]
 
-    [view {:style {:flex         1
-                   :flex-direction "column"
+    [view {:style {:flex            1
+                   :flex-direction  "column"
                    :justify-content "flex-start"
-                   :align-items "flex-start"
-                   :padding-top  50
-                   :padding-left 10}}
+                   :align-items     "flex-start"
+                   :padding-top     50
+                   :padding-left    10}}
      [view {:style {:flex-direction "row"}}
       [text {:style {:color         "grey"
                      :padding-right 5}} ":id"]
@@ -95,7 +123,7 @@
                     :align-items    "center"}}
       [text {:style {:color         "grey"
                      :padding-right 5}} ":label"]
-      [text-input {:placeholder    (:label task)
+      [text-input {:default-value  (:label task)
                    :style          {:height 40
                                     :width  200}
                    :spell-check    true
@@ -106,12 +134,12 @@
 
      ;; :data        map?
      ;; https://clojuredocs.org/clojure.walk/walk
-     [view {:style {:flex 1
+     [view {:style {:flex           1
                     :flex-direction "row"
                     :align-items    "flex-start"}}
       [text {:style {:color         "grey"
                      :padding-right 5}} ":data"]
-      (structured-data [] (:data task))]
+      (structured-data [:map-of-stuff] (:data task))]
 
      ;; :created     ::moment ;; can't edit display date in their time zone
      ;; :last-edited ::moment ;; can't edit display date in their time zone
