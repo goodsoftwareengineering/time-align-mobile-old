25b7184a5ca5ae4d0cea14f18218113d425c51a1
justin good <jgoodhcg@gmail.com>
2018-07-15 13:30:32 -0400

    Add form change indicators

--- src/time_align_mobile/screens/task_form.cljs
+++ src/time_align_mobile/screens/task_form.cljs
@@ -10,7 +10,8 @@
 
 (defn root [params]
   (let [task-form              (subscribe [:get-task-form])
-        update-structured-data (fn [new-data] (dispatch [:update-task-form {:data new-data}]))]
+        update-structured-data (fn [new-data] (dispatch [:update-task-form {:data new-data}]))
+        changes                (subscribe [:get-task-form-changes])]
 
     [keyboard-aware-scroll-view
      ;; check link for why these options https://stackoverflow.com/questions/45466026/keyboard-aware-scroll-view-android-issue?rq=1
@@ -29,7 +30,9 @@
 
       [view {:style {:flex-direction "row"
                      :align-items    "center"}}
-       [text {:style {:color         "grey"
+       [text {:style {:color         (if (contains? @changes :label)
+                                       "blue"
+                                       "grey")
                       :padding-right 5}} ":label"]
        [text-input {:default-value  (:label @task-form)
                     :style          {:height 40
@@ -46,21 +49,26 @@
       [view {:style {:flex           1
                      :flex-direction "row"
                      :align-items    "flex-start"}}
-       [text {:style {:color         "grey"
+       [text {:style {:color         (if (contains? @changes :data)
+                                       "blue"
+                                       "grey")
                       :padding-right 5}} ":data"]
        [structured-data {:data   (:data @task-form)
                          :update update-structured-data}]]
 
 
-      [view {:style {:flex 1
-                     :flex-direction "row"
-                     :align-items "center"
+      [view {:style {:flex            1
+                     :flex-direction  "row"
+                     :align-items     "center"
                      :justify-content "center"}}
        [touchable-highlight {:on-press #(dispatch [:save-task-form])
-                             :style {:padding 5
-                                     :margin-right 10}}
+                             :style    {:padding      5
+                                        :margin-right 10}}
         [text "save"]]
-       [touchable-highlight {:on-press #(dispatch [:load-task-form (:id @task-form)])}
+       [touchable-highlight {:on-press #(dispatch [:load-task-form (:id @task-form)])
+                             :style (if (< 0 (count (keys @changes)))
+                                      {:background-color "grey"}
+                                      {})}
         [text "cancel"]]]
       ;; :created     ::moment ;; can't edit display date in their time zone
       ;; :last-edited ::moment ;; can't edit display date in their time zone
--- src/time_align_mobile/subs.cljs
+++ src/time_align_mobile/subs.cljs
@@ -1,9 +1,9 @@
 (ns time-align-mobile.subs
-  (:require [re-frame.core :refer [reg-sub]]))
+  (:require [re-frame.core :refer [reg-sub]]
+            [com.rpl.specter :as sp :refer-macros [select setval transform]]))
 
 (defn get-navigation [db _]
   (get-in db [:navigation]))
-(reg-sub :get-navigation get-navigation)
 
 (defn get-task-form [db _]
   (let [task-form (get-in db [:view :task-form])]
@@ -12,6 +12,26 @@
       {:id "nothing"
        :label "here yet"
        :data {:please "wait"}})))
+
+(defn get-task-form-changes [db _]
+  (let [task-form (get-in db [:view :task-form])]
+    (if (some? (:id task-form))
+      (let [task (first
+                  (select [:tasks sp/ALL #(= (:id %) (:id task-form))]
+                          db))
+            ;; data needs to be coerced to compare to form
+            new-data (.stringify js/JSON
+                                 (clj->js (:data task))
+                                 nil 2)
+            altered-task (merge task {:data new-data})
+            different-keys (->> (clojure.data/diff task-form altered-task)
+                                (first))]
+        different-keys)
+      ;; return an empty map if there is no loaded task in the form
+      {})))
+
+(reg-sub :get-navigation get-navigation)
 (reg-sub :get-task-form get-task-form)
+(reg-sub :get-task-form-changes get-task-form-changes)
 
 
