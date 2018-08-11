2da681348f5860cf09a2c50bdda29e641ef1d73f
justin good <jgoodhcg@gmail.com>
2018-08-11 17:51:21 -0400

    Fix indentation on core drawer

--- src/time_align_mobile/core.cljs
+++ src/time_align_mobile/core.cljs
@@ -47,14 +47,23 @@
                                                    (println {:current-screen id
                                                              :params         nil})
                                                    ;; TODO remove bucket id params when done testing
-                                                   (dispatch [:navigate-to {:current-screen id
-                                                                            :params
-                                                                            (cond
-                                                                              (= id :bucket)   {:bucket-id (uuid "a7396f81-38d4-4d4f-ab19-a7cef18c4ea2")}
-                                                                              (= id :period)   {:period-id (uuid "a8404f81-38d4-4d4f-ab19-a7cef18c4531")}
-                                                                              (= id :template) {:template-id (uuid "c52e4f81-38d4-4d4f-ab19-a7cef18c8882")}
-                                                                              (= id :filter)   {:filter-id (uuid "bbc34081-38d4-4d4f-ab19-a7cef18c1212")}
-                                                                              :else            nil)}]))}
+                                                   (dispatch
+                                                    [:navigate-to
+                                                     {:current-screen id
+                                                      :params
+                                                      (cond
+                                                        (= id :bucket)
+                                                        {:bucket-id (uuid "a7396f81-38d4-4d4f-ab19-a7cef18c4ea2")}
+
+                                                        (= id :period)
+                                                        {:period-id (uuid "a8404f81-38d4-4d4f-ab19-a7cef18c4531")}
+
+                                                        (= id :template)
+                                                        {:template-id (uuid "c52e4f81-38d4-4d4f-ab19-a7cef18c8882")}
+
+                                                        (= id :filter)
+                                                        {:filter-id (uuid "bbc34081-38d4-4d4f-ab19-a7cef18c1212")}
+                                                        :else            nil)}]))}
                   [view {:flex-direction  "row"
                          :justify-content "flex-start"
                          :align-items     "center"
