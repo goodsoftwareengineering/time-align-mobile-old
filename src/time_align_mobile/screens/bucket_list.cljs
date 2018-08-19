c835f64d3a23c122b35c3fc5facea5fee73e5e34
justin good <jgoodhcg@gmail.com>
2018-08-19 11:35:12 -0400

    Add bucket list

--- src/time_align_mobile/screens/bucket_list.cljs
+++ src/time_align_mobile/screens/bucket_list.cljs
@@ -1,6 +1,47 @@
 (ns time-align-mobile.screens.bucket-list
-  (:require [time-align-mobile.js-imports :refer [view text]] ))
+  (:require [time-align-mobile.js-imports :refer [view
+                                                  text
+                                                  flat-list
+                                                  touchable-highlight]]
+            [time-align-mobile.components.filter-picker :refer [filter-picker
+                                                                filter-items]]
+            [reagent.core :as r :refer [atom]]
+            [re-frame.core :refer [subscribe dispatch]]))
 
 (defn root [params]
-  [view {:style {:flex 1 :justify-content "center" :align-items "center"}}
-   [text "task list"]])
+  (let [buckets (subscribe [:get-buckets])
+        active-filter (subscribe [:get-active-filter])]
+    [view {:style {:flex 1 :justify-content "center" :align-items "center"}}
+     [filter-picker]
+     [flat-list {:data (filter-items @buckets @active-filter)
+                 :render-item
+                 (fn [i]
+                   (let [item (:item (js->clj i :keywordize-keys true))
+                         id (:id item)
+                         label (:label item)
+                         periods (:periods item)
+                         templates (:templates item)
+                         color (:color item)]
+                     (r/as-element [touchable-highlight
+                                    {:key id
+                                     :on-press #(dispatch
+                                                 [:navigate-to
+                                                  {:current-screen :bucket
+                                                   :params {:bucket-id id}}])}
+
+                                    [view {:style {:flex-direction "row"}}
+                                     [view
+                                      {:style {:width 50
+                                               :height 50
+                                               :margin-right 20
+                                               :background-color color}}]
+                                     [view {:style {:flex-direction "column"}}
+                                      [text (if (> (count label) 0)
+                                              label
+                                              "No label")]
+                                      [text {:style {:color "grey"}}
+                                       "periods: " (count periods)]
+                                      [text {:style {:color "grey"}}
+                                       "templates: " (count templates)]
+                                      [text {:style {:color "grey"}}
+                                       (str "id: " id)]]]])))}]]))
