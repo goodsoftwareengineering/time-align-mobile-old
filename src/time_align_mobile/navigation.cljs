23867b0e6e23d430e4e35a3737f2acfbadb43c3c
justin good <jgoodhcg@gmail.com>
2018-08-26 11:29:37 -0400

    Refactor navigation ns

--- src/time_align_mobile/navigation.cljs
+++ src/time_align_mobile/navigation.cljs
@@ -9,110 +9,115 @@
             [time-align-mobile.screens.bucket-list :as bucket-list-screen]
             [time-align-mobile.screens.template-form :as template-form-screen]
             [time-align-mobile.screens.template-list :as template-list-screen]
-            [time-align-mobile.screens.filter-form :as filter-form-screen]))
+            [time-align-mobile.screens.filter-form :as filter-form-screen]
+            [time-align-mobile.screens.filter-list :as filter-list-screen]))
+
+(def bucket {:id              :bucket
+             :label           "bucket"
+             :screen          bucket-form-screen/root
+             :in-drawer       true
+             :position-drawer 999
+             :icon            {:family "Entypo"
+                               :name "credit"}})
+
+(def buckets {:id              :buckets
+              :label           "buckets"
+              :screen          bucket-list-screen/root
+              :in-drawer       true
+              :position-drawer 5
+              :icon            {:family "FontAwesome"
+                                :name   "list"}})
+
+(def period {:id              :period
+             :label           "period"
+             :screen          period-form-screen/root
+             :in-drawer       true
+             :position-drawer 998
+             :icon            {:family "Entypo"
+                               :name "credit"}})
+
+(def periods {:id                  :periods
+              :label           "periods"
+              :screen          period-list-screen/root
+              :in-drawer       true
+              :position-drawer 2
+              :icon            {:family "Entypo"
+                                :name   "time-slot"}})
+
+(def template {:id              :template
+               :label           "template"
+               :screen          template-form-screen/root
+               :in-drawer       true
+               :position-drawer 997
+               :icon            {:family "Entypo"
+                                 :name   "credit"}})
+
+(def templates {:id              :templates
+                :label           "templates"
+                :screen          template-list-screen/root
+                :in-drawer       true
+                :position-drawer 6
+                :icon            {:family "FontAwesome"
+                                  :name   "wpforms"}})
+
+(def _filter {:id              :filter
+             :label           "filter"
+             :screen          filter-form-screen/root
+             :in-drawer       true
+             :position-drawer 997
+             :icon            {:family "Entypo"
+                               :name   "credit"}})
+
+(def _filters {:id             :filters
+              :label           "filters"
+              :screen          filter-list-screen/root
+              :in-drawer       true
+              :position-drawer 997
+              :icon            {:family "Entypo"
+                                :name   "credit"}})
+
+(def day {:id              :day
+          :label           "day"
+          :screen          day-screen/root
+          :in-drawer       true
+          :position-drawer 1
+          :icon            {:family "FontAwesome"
+                            :name   "columns"}})
+
+(def calendar {:id              :calendar
+               :label           "calendar"
+               :screen          calendar-screen/root
+               :in-drawer       true
+               :position-drawer 0
+               :icon            {:family "Entypo"
+                                 :name   "calendar"}})
+
+(def report {:id              :report
+             :label           "report"
+             :screen          report-screen/root
+             :in-drawer       true
+             :position-drawer 4
+             :icon            {:family "Entypo"
+                               :name   "bar-graph"}})
+
+(def queue {:id              :queue
+            :label           "queue"
+            :screen          queue-screen/root
+            :in-drawer       true
+            :position-drawer 3
+            :icon            {:family "MaterialIcons"
+                              :name   "queue"}})
 
 ;; https://expo.github.io/vector-icons/
-(def screens-map [{:id              :calendar
-                   :label           "calendar"
-                   :screen          calendar-screen/root
-                   :in-drawer       true
-                   :position-drawer 0
-                   :icon            {:family "Entypo"
-                                     :name   "calendar"}}
-
-                  {:id              :day
-                   :label           "day"
-                   :screen          day-screen/root
-                   :in-drawer       true
-                   :position-drawer 1
-                   :icon            {:family "FontAwesome"
-                                     :name   "columns"}}
-
-                  {:id              :period
-                   :label           "period"
-                   :screen          period-form-screen/root
-                   :in-drawer       false
-                   :position-drawer nil
-                   :icon            nil}
-
-                  {:id              :periods
-                   :label           "periods"
-                   :screen          period-list-screen/root
-                   :in-drawer       true
-                   :position-drawer 2
-                   :icon            {:family "Entypo"
-                                     :name   "time-slot"}}
-
-                  {:id              :queue
-                   :label           "queue"
-                   :screen          queue-screen/root
-                   :in-drawer       true
-                   :position-drawer 3
-                   :icon            {:family "MaterialIcons"
-                                     :name   "queue"}}
-
-                  {:id              :report
-                   :label           "report"
-                   :screen          report-screen/root
-                   :in-drawer       true
-                   :position-drawer 4
-                   :icon            {:family "Entypo"
-                                     :name   "bar-graph"}}
-
-                  ;; TODO negate :in-drawer when done testing forms
-                  {:id              :bucket
-                   :label           "bucket"
-                   :screen          bucket-form-screen/root
-                   :in-drawer       true
-                   :position-drawer 999
-                   :icon            {:family "Entypo"
-                                     :name "credit"}}
-
-                  {:id              :period
-                   :label           "period"
-                   :screen          period-form-screen/root
-                   :in-drawer       true
-                   :position-drawer 998
-                   :icon            {:family "Entypo"
-                                     :name "credit"}}
-
-                  {:id              :template
-                   :label           "template"
-                   :screen          template-form-screen/root
-                   :in-drawer       true
-                   :position-drawer 997
-                   :icon            {:family "Entypo"
-                                     :name "credit"}}
-
-                  {:id              :filter
-                   :label           "filter"
-                   :screen          filter-form-screen/root
-                   :in-drawer       true
-                   :position-drawer 997
-                   :icon            {:family "Entypo"
-                                     :name "credit"}}
-
-                  ;; />
-
-                  {:id              :buckets
-                   :label           "buckets"
-                   :screen          bucket-list-screen/root
-                   :in-drawer       true
-                   :position-drawer 5
-                   :icon            {:family "FontAwesome"
-                                     :name   "list"}}
-
-                  {:id              :template
-                   :label           "template"
-                   :screen          template-form-screen/root
-                   :in-drawer       false
-                   :position-drawer nil
-                   :icon            nil}
-
-                  {:id              :templates
-                   :label           "templates"
-                   :screen          template-list-screen/root
-                   :in-drawer       true
-                   :position-drawer 6
-                   :icon            {:family "FontAwesome"
-                                     :name   "wpforms"}}])
+(def screens-map [bucket
+                  buckets
+                  period
+                  periods
+                  template
+                  templates
+                  _filter
+                  _filters
+                  day
+                  calendar
+                  report
+                  queue])
