0f1b4df20254afe75ade4713146f23b415fe1f8b
justin good <jgoodhcg@gmail.com>
2018-09-22 14:43:23 -0400

    Add a view that knows its own height

--- src/time_align_mobile/screens/day.cljs
+++ src/time_align_mobile/screens/day.cljs
@@ -1,6 +1,20 @@
 (ns time-align-mobile.screens.day
-  (:require [time-align-mobile.js-imports :refer [view text]] ))
+  (:require [time-align-mobile.js-imports :refer [view text]]
+            [reagent.core :as r]))
+
+(def dimensions (r/atom {:width nil :height nil}))
 
 (defn root [params]
-  [view {:style {:flex 1 :justify-content "center" :align-items "center"}}
-   [text "day"]])
+  [view {:style     {:flex 1 :justify-content "center" :align-items "center"}
+         :on-layout (fn [event]
+                      (let [layout (-> event
+                                       (.-nativeEvent)
+                                       (.-layout)
+                                       (js->clj :keywordize-keys true))]
+                        (if (nil? (:height dimensions))
+                          (reset! dimensions {:width  (:width layout)
+                                              :height (:height layout)}))))}
+
+   [view {:style {:height           (:height @dimensions)
+                  :width            (:width @dimensions)
+                  :background-color "green"}}]])
