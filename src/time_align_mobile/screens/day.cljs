e6322e6b3d8756321d0d787b43692c5f4b0e8a9b
justin good <jgoodhcg@gmail.com>
2018-10-04 08:04:10 -0400

    Add button in the middle

--- src/time_align_mobile/screens/day.cljs
+++ src/time_align_mobile/screens/day.cljs
@@ -1,6 +1,7 @@
 (ns time-align-mobile.screens.day
   (:require [time-align-mobile.js-imports :refer [view
                                                   text
+                                                  touchable-highlight
                                                   animated-view
                                                   animated-xy
                                                   pan-responder]]
@@ -27,4 +28,24 @@
 
          [view {:style {:height           (:height @dimensions)
                         :width            (:width @dimensions)
-                        :background-color "blue"}}]])})))
+                        :background-color "blue"}}
+          [touchable-highlight
+           {:on-press (fn [e]
+                        (println e)
+                        ;; (let [synthetic-event (js->clj e :keywordize-keys true)]
+                        ;;   (println synthetic-event))
+                        )
+            :style    {:top              (-> @dimensions
+                                              (:height)
+                                              (/ 2)
+                                              (- 25))
+                       :left             (-> @dimensions
+                                             (:width)
+                                             (/ 2)
+                                             (- 25))
+                       :width            50
+                       :height           50}}
+           [view {:style {:width "100%"
+                          :height "100%"
+                          :border-radius    10
+                          :background-color "green"}}]]]])})))
