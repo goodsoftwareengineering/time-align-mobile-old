edc81cb5253e7535c1e2dcc29012c95b1570ca8a
justin good <jgoodhcg@gmail.com>
2018-10-08 07:34:04 -0400

    :no_entry_sign: Remove pan-responder

--- src/time_align_mobile/screens/day.cljs
+++ src/time_align_mobile/screens/day.cljs
@@ -26,51 +26,13 @@
    [text {:style {:justify-content "center"
                   :align-items "center"}} (str (js/Date.))]])
 
-(defn set-pan-responder [{:keys [pan-responder y-pos top-bar-height pan dimensions]}]
-  (println "Will mount...")
-  (let [pr (ocall pan-responder "create"
-                  (clj->js {:onStartShouldSetPanResponder #(do (println "onStartShouldSetPanResponder called") true)
-                            :onMoveShouldSetPanResponder  #(do (println "onMoveShouldSetPanResponder called") true)
-
-                            :onPanResponderGrant     #(println "onPanResponderGrant called..")
-                            :onPanResponderMove      #(do
-                                                        (let [new-y    (- (get (js->clj %2) "moveY")
-                                                                          top-bar-height)
-                                                              y-ms     (-> new-y
-                                                                           (/ (:height @dimensions))
-                                                                           (* day-ms))
-                                                              y-hour   (-> y-ms
-                                                                           (/ 1000)
-                                                                           (/ 60)
-                                                                           (/ 60))
-                                                              target   (oget %1 "target")
-                                                              instance (get-instance-from-node target)
-                                                              key      (oget instance "key")]
-                                                          (println key)
-                                                          (if (< y-hour 23.99)
-                                                            (reset! y-pos new-y)
-                                                            (println "Move it to the next day"))))
-                            :onPanResponderRelease   #(println "onPanResponderRelease called..")
-                            :onPanResponderTerminate #(println "onPanResponderTerminate called..")}))]
-          (reset! pan pr)))
-
 (defn root [params]
   (let [dimensions     (r/atom {:width nil :height nil})
-        top-bar-height 25
-        y-pos          (r/atom 0)
-        pan            (r/atom nil)]
+        top-bar-height 25]
     (r/create-class
-     {:component-will-mount
-      #(set-pan-responder {:pan-responder  pan-responder
-                           :y-pos          y-pos
-                           :top-bar-height top-bar-height
-                           :pan            pan
-                           :dimensions     dimensions})
-
-      :reagent-render
+     {:reagent-render
       (fn [params]
-        (let [
-              square-width  100
+        (let [square-width  100
               start         0
               stop          (* 6 60 60 1000) ;; 3 hours in millis
               square-height (-> (- stop start)
@@ -95,23 +57,22 @@
            [view {:style {:height           (:height @dimensions) ;; this is already adjusted to accoutn for top-bar
                           :width            (:width @dimensions)
                           :background-color "blue"}}
-            [view (merge (js->clj (oget @pan "panHandlers"))
-                         {:id    "this-is-my-node"
-                          :key   "this-is-the-key"
-                          :style {:top              (->> @y-pos
-                                                         (max 0)
-                                                         (min (:height @dimensions)))
-                                  :left             (-> @dimensions
-                                                        (:width)
-                                                        (/ 2)
-                                                        (- (/ square-width 2)))
-                                  :width            square-width
-                                  :height           square-height
-                                  :border-radius    10
-                                  :background-color "yellow"}})
-             [text (str "starting at " (-> @y-pos
-                                           (/ (:height @dimensions))
-                                           (* day-ms)
+            [view {:id    "this-is-my-node"
+                   :key   "this-is-the-key"
+                   :style {:top              (-> start
+                                                 (/ day-ms)
+                                                 (* (:height @dimensions))
+                                                 (max 0)
+                                                 (min (:height @dimensions)))
+                           :left             (-> @dimensions
+                                                 (:width)
+                                                 (/ 2)
+                                                 (- (/ square-width 2)))
+                           :width            square-width
+                           :height           square-height
+                           :border-radius    10
+                           :background-color "yellow"}}
+             [text (str "starting at " (-> start
                                            (/ 1000)
                                            (/ 60)
                                            (/ 60)
