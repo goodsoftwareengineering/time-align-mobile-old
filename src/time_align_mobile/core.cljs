8fa17fc2f92b9cf9b8b6504b6c39f6b2b47bef53
justin good <jgoodhcg@gmail.com>
2018-12-01 15:55:49 -0500

    Simplify tick dispatch

--- src/time_align_mobile/core.cljs
+++ src/time_align_mobile/core.cljs
@@ -93,7 +93,5 @@
 (defn init []
   (dispatch-sync [:initialize-db])
   ;; Start ticking
-  (js/setInterval (fn []
-                    (let [time (js/Date.)]
-                      (dispatch [:tick time]))) 1000)
+  (js/setInterval #(dispatch [:tick (js/Date.)]) 1000)
   (ocall expo "registerRootComponent" (r/reactify-component app-root)))
