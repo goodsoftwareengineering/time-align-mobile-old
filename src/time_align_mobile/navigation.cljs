540ed060b7326b8b995706bd2c9c4c1472cedf37
justin good <jgoodhcg@gmail.com>
2018-12-03 18:45:18 -0500

    Format navigation

--- src/time_align_mobile/navigation.cljs
+++ src/time_align_mobile/navigation.cljs
@@ -12,97 +12,97 @@
             [time-align-mobile.screens.filter-form :as filter-form-screen]
             [time-align-mobile.screens.filter-list :as filter-list-screen]))
 
-(def bucket-screen {:id              :bucket
-             :label           "bucket"
-             :screen          bucket-form-screen/root
-             :in-drawer       false
-             :position-drawer 999
-             :icon            nil})
+(def bucket-screen {:id       :bucket
+                    :label           "bucket"
+                    :screen          bucket-form-screen/root
+                    :in-drawer       false
+                    :position-drawer 999
+                    :icon            nil})
 
-(def buckets-screen {:id              :buckets
-              :label           "buckets"
-              :screen          bucket-list-screen/root
-              :in-drawer       true
-              :position-drawer 3
-              :icon            {:family "FontAwesome"
-                                :name   "list"}})
+(def buckets-screen {:id       :buckets
+                     :label           "buckets"
+                     :screen          bucket-list-screen/root
+                     :in-drawer       true
+                     :position-drawer 3
+                     :icon            {:family "FontAwesome"
+                                       :name   "list"}})
 
-(def period-screen {:id              :period
-             :label           "period"
-             :screen          period-form-screen/root
-             :in-drawer       false
-             :position-drawer nil
-             :icon            nil})
+(def period-screen {:id       :period
+                    :label           "period"
+                    :screen          period-form-screen/root
+                    :in-drawer       false
+                    :position-drawer nil
+                    :icon            nil})
 
-(def periods-screen {:id                  :periods
-              :label           "periods"
-              :screen          period-list-screen/root
-              :in-drawer       true
-              :position-drawer 4
-              :icon            {:family "Entypo"
-                                :name   "time-slot"}})
+(def periods-screen {:id       :periods
+                     :label           "periods"
+                     :screen          period-list-screen/root
+                     :in-drawer       true
+                     :position-drawer 4
+                     :icon            {:family "Entypo"
+                                       :name   "time-slot"}})
 
-(def template-screen {:id              :template
-               :label           "template"
-               :screen          template-form-screen/root
-               :in-drawer       false
-               :position-drawer nil
-               :icon            nil})
+(def template-screen {:id       :template
+                      :label           "template"
+                      :screen          template-form-screen/root
+                      :in-drawer       false
+                      :position-drawer nil
+                      :icon            nil})
 
-(def templates-screen {:id              :templates
-                :label           "templates"
-                :screen          template-list-screen/root
-                :in-drawer       true
-                :position-drawer 5
-                :icon            {:family "FontAwesome"
-                                  :name   "wpforms"}})
+(def templates-screen {:id       :templates
+                       :label           "templates"
+                       :screen          template-list-screen/root
+                       :in-drawer       true
+                       :position-drawer 5
+                       :icon            {:family "FontAwesome"
+                                         :name   "wpforms"}})
 
-(def filter-screen {:id              :filter
-             :label           "filter"
-             :screen          filter-form-screen/root
-             :in-drawer       false
-             :position-drawer nil
-             :icon            nil})
+(def filter-screen {:id       :filter
+                    :label           "filter"
+                    :screen          filter-form-screen/root
+                    :in-drawer       false
+                    :position-drawer nil
+                    :icon            nil})
 
-(def filters-screen {:id             :filters
-              :label           "filters"
-              :screen          filter-list-screen/root
-              :in-drawer       true
-              :position-drawer 6
-              :icon            {:family "FontAwesome"
-                                :name   "filter"}})
+(def filters-screen {:id       :filters
+                     :label           "filters"
+                     :screen          filter-list-screen/root
+                     :in-drawer       true
+                     :position-drawer 6
+                     :icon            {:family "FontAwesome"
+                                       :name   "filter"}})
 
-(def day-screen {:id              :day
-          :label           "day"
-          :screen          day-screen/root
-          :in-drawer       true
-          :position-drawer 1
-          :icon            {:family "FontAwesome"
-                            :name   "columns"}})
+(def day-screen {:id       :day
+                 :label           "day"
+                 :screen          day-screen/root
+                 :in-drawer       true
+                 :position-drawer 1
+                 :icon            {:family "FontAwesome"
+                                   :name   "columns"}})
 
-(def calendar-screen {:id              :calendar
-               :label           "calendar"
-               :screen          calendar-screen/root
-               :in-drawer       true
-               :position-drawer 2
-               :icon            {:family "Entypo"
-                                 :name   "calendar"}})
+(def calendar-screen {:id       :calendar
+                      :label           "calendar"
+                      :screen          calendar-screen/root
+                      :in-drawer       true
+                      :position-drawer 2
+                      :icon            {:family "Entypo"
+                                        :name   "calendar"}})
 
-(def report-screen {:id              :report
-             :label           "report"
-             :screen          report-screen/root
-             :in-drawer       true
-             :position-drawer 8
-             :icon            {:family "Entypo"
-                               :name   "bar-graph"}})
+(def report-screen {:id       :report
+                    :label           "report"
+                    :screen          report-screen/root
+                    :in-drawer       true
+                    :position-drawer 8
+                    :icon            {:family "Entypo"
+                                      :name   "bar-graph"}})
 
-(def queue-screen {:id              :queue
-            :label           "queue"
-            :screen          queue-screen/root
-            :in-drawer       true
-            :position-drawer 7
-            :icon            {:family "MaterialIcons"
-                              :name   "queue"}})
+(def queue-screen {:id       :queue
+                   :label           "queue"
+                   :screen          queue-screen/root
+                   :in-drawer       true
+                   :position-drawer 7
+                   :icon            {:family "MaterialIcons"
+                                     :name   "queue"}})
 
 ;; https://expo.github.io/vector-icons/
 (def screens-map [bucket-screen
